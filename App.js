0ef210c6fc3d4da1f697f49576e006cf1df05d0b
Albertohf <albertohf91@gmail.com>
2023-12-11 19:20:08 -0300

    add logo

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import Home from './src/pages/Home'
+import { Image } from 'react-native';
+
 
 export default function App() {
   return (
     <>
-      <Home /> 
+      <Home />
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import Slider from '@react-native-community/slider'
+import { useState } from 'react';
+
 
 export default function Home() {
+    const [value, setValue] = useState(6);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.logo} source={require('../assets/logo.png')} />
+      <Text style={styles.title}>{value} Caracteres</Text>
+
+      <View style={styles.area}>
+        <Slider
+          style={{ width: '100%' }}
+          minimumValue={6}
+          maximumValue={20}
+          minimumTrackTintColor="#00ff00"
+          maximumTrackTintColor="#000000"
+          thumbTintColor='#00ff'
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button} opacity={.7}>
+        <Text style={styles.buttonText}>Gerar Senha</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +36,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: '#00ff00',
+    width: '80%',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  title: {
+    fontSize: 35,
+    color: '#00ff',
+  },
+  logo: {
+    marginBottom: 60,
+  }, 
+  area: {
+    marginBottom: 14,
+    marginTop: 20,
+    width: '80%',
+    padding: 12,
+  }
 });
